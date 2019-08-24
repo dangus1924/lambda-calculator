c37fd76e7f7d7878c2885ec2d39fb8e19e0d9dab
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-08-24 12:57:33 -0400

    added the display

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  
   return (
-    <>
+    <button className="specialButton"
+    onClick={() =>
+    props.clearDisplay}>
+    {props.text}    
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default SpecialButton;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className="display">{/* Display any props data here */}
+  <p className="output">
+    {props.number}
+    </p>
+    </div>;
 };
+
+export default Display;
