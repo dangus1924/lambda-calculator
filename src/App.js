dbbd025beae691287cfc28d8e1e0209fee729eb6
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-08-24 13:26:32 -0400

    numbers and special character poped up on the scree

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from './components/DisplayComponents/Display';
 // import Display from "./components/DisplayComponents/Display"
 
 
@@ -11,6 +12,7 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import { operators, specials } from "./data";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -19,11 +21,33 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const[displayValue, setDisplayValue] = useState('');
+  const clearDisplay = (special) => {
+    if (special === "C") {
+      setDisplayValue(displayValue => (" "))
+    } else
+      setDisplayValue(displayValue);
+  }
+
+const addOperator = (operator) => {
+  if (operator === "=") {
+    setDisplayValue(displayValue => eval(displayValue))
+  } else {
+    setDisplayValue(displayValue => displayValue + " " + operator + " ")
+  }
+}
+
+const addNumber = (number) => {
+  setDisplayValue(displayValue => displayValue + number);
+}
   return (
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+      <Display number={displayValue} />
+      <Numbers addNumber={addNumber} />
+      <Specials clearDisplay={clearDisplay}/>
+      <Operators addOperator={addOperator} />
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Display = () => {
+const Display = (props) => {
   return <div className="display">{/* Display any props data here */}
   <p className="output">
     {props.number}
